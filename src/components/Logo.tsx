e7b5b87c7f692ea6399075056a1afe775ee17ea0
Fiber0 <zelva3000@gmail.com>
2024-11-27 19:39:08 +0100

    fixed logo resizing with page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 function Logo() {
-    return <img src="logo_metlicka.png" alt="React Logo" width={"2%"}/>
+    return <img src="logo_metlicka.png" alt="React Logo" width={"45px"}/>
 }
 
 export default Logo
