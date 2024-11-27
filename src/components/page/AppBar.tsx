377259026768ea9d4434fdd926c1f3b5aacbc724
Fiber0 <zelva3000@gmail.com>
2024-11-27 20:02:30 +0100

    fixed bar text overflow

--- src/components/page/AppBar.tsx
+++ src/components/page/AppBar.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {Box, Button} from "@mui/material";
+import {Box, Button, useMediaQuery} from "@mui/material";
+import articleTheme from "../../themes/ArticleTheme.ts";
 import Logo from "../Logo.tsx";
 
 const MainBar: React.FC = () => {
+    const isMobile = useMediaQuery(articleTheme.breakpoints.down('sm'));
+    let textSize = isMobile ? 12 : 17;
     return (
         <AppBar position="fixed" sx={{ top: 0 }}>
             <Toolbar sx={{display: "flex", gap: "1em"}}>
                 <Logo/>
-                <Typography variant="h6">
+                <Typography variant="h6" sx={{ fontSize: textSize }}>
                     Vavřinecká Metlička
                 </Typography>
                 <Box sx={{flexGrow: 3, display: "flex", gap: "1em"}}/>
-                <Button color="inherit">Přehled Akcí</Button>
-                <Button color="inherit">O Metličce</Button>
+                <Button color="inherit" sx={{ fontSize: textSize }}>Přehled Akcí</Button>
+                <Button color="inherit" sx={{ fontSize: textSize }}>O Metličce</Button>
             </Toolbar>
         </AppBar>
     );
